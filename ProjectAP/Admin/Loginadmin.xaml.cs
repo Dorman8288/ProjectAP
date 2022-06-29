bc931b63320f0db3c78aab8ad8615be11a2eab99
mm bardal <bardal.mohammadmahdi@gmail.com>
2022-06-29 10:49:40 +0430

    add login for admin

---
+++ ProjectAP/Admin/Loginadmin.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectAP.Admin
+{
+    /// <summary>
+    /// Interaction logic for Loginadmin.xaml
+    /// </summary>
+    public partial class Loginadmin : Page
+    {
+        public Loginadmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
