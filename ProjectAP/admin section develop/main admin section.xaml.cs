0f62bfc375efcebcc5e39d404b693dba95e1478f
mm bardal <bardal.mohammadmahdi@gmail.com>
2022-07-01 11:35:17 +0430

    admin section develpe

---
+++ ProjectAP/admin section develop/main admin section.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectAP.admin_section_develop
+{
+    /// <summary>
+    /// Interaction logic for main_admin_section.xaml
+    /// </summary>
+    public partial class main_admin_section : Page
+    {
+        public main_admin_section()
+        {
+            InitializeComponent();
+        }
+    }
+}
