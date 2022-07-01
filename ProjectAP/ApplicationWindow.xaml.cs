8678aad6d92a700867bb6b01160d34619f229330
Dorman <dorman8288@gmail.com>
2022-07-01 11:28:28 +0430

    test

--- ProjectAP/ApplicationWindow.xaml.cs
+++ ProjectAP/ApplicationWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,19 +8,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ProjectAP
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ApplicationWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ApplicationWindow : Window
     {
-        public MainWindow()
+        public ApplicationWindow()
         {
             InitializeComponent();
         }
+
     }
 }
